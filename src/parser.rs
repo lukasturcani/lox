5f63c22b6816514a7828618f81c525065d21e974
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-22 10:52:13 +0100

    wip

---
+++ src/parser.rs
@@ -0,0 +1,14 @@
+use crate::scanner::Token;
+
+enum Expr {
+    Assign {
+        name: Token,
+        value: Box<Expr>,
+    },
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Call,
+}
